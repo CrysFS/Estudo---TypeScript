93f5d79f9b7433fe9a6990967862a1a1675c7dcf
CrysFS <crystianschlemper@gmail.com>
2021-10-26 22:07:51 -0300

    study syntax

---
+++ syntax/index.js
@@ -0,0 +1,23 @@
+const canfly = false
+const birdname = 'Licador'
+const birdage = 99
+
+console.log('Pode voar?',canfly)
+console.log('Nome do Passáro:',birdname)
+console.log('Idade do Passáro:',birdage)
+
+const dino = {
+    age: 10,
+    fly: 'n pode',
+    name: 'dino'
+}
+
+console.log(dino.name)
+
+const myarr = ["ok", "ko", "td", "dt","t"]
+
+console.log(myarr[3])
+
+const tarr = [{age: 10, name: "kaka"}, {age: 8, name: 'toto'}, {age: 6, name: 'girl'}]
+
+console.log(tarr[0].age, tarr[2].name)
